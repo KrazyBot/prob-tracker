780fc4308373d1a2f71f336292542a4b1f0f75ab
root <root@localhost.localdomain>
2019-05-20 12:04:57 +0100

    bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Prob-Tacker' });
 });
 
 module.exports = router;

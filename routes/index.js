2908b60140aa6da6b891a027b20705dc4b154175
root <root@localhost.localdomain>
2019-05-20 15:26:18 +0100

    General layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Prob-Tacker' });
+  res.render('index', { title: 'Problem-Tacker' });
 });
 
 module.exports = router;
